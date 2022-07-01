829eff72f82093c3a4cab869b8532fe4b53590c8
RiyaDominic <108110518+RiyaDominic@users.noreply.github.com>
2022-07-01 03:14:56 -0700

    Add files via upload

---
+++ inheri.py
@@ -0,0 +1,10 @@
+class Student:  
+    def name(self):  
+        print("Riya Dominic")  
+#child class Dog inherits the base class Animal  
+class Riya(Student):  
+    def place(self):  
+        print("Cherupuzha")  
+d = Riya()  
+d.name()  
+d.place()  
